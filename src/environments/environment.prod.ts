f6d9f0b5a76f82f5b5aa574888616d2e0b202ce8
abdallahshaaban <abdallahshaaban135@gmail.com>
2021-05-17 11:09:14 +0300

    setup api url in env.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiURL:"http://localhost:3000/"
+   /*how to run this localhost through cmd `json-server --watch db.json`
+  for more info visit https://www.npmjs.com/package/json-server */
+};
