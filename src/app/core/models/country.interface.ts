d80eaccb6c25afc239776d81e83aa1ebe29f19fe
abdallahshaaban <abdallahshaaban135@gmail.com>
2021-05-17 11:13:26 +0300

    hero model + country model

---
+++ src/app/core/models/country.interface.ts
@@ -0,0 +1,17 @@
+export interface CountryInterface {
+  name: string;
+  alpha2code: string;
+  alpha3code: string;
+  nativeName: string;
+  region: string;
+  subRegion: string;
+  latitude: string;
+  longitude: string;
+  area: number;
+  numericCode: number;
+  nativeLanguage: string;
+  currencyCode: string;
+  currencyName: number;
+  currencySymbol: string;
+  flag: string;
+}
---
+++ src/app/core/models/hero.interface.ts
@@ -0,0 +1,10 @@
+export interface HeroInterface {
+  user_name: string;
+  user_phone: string;
+  email: string;
+  date: string;
+  country: string;
+  subRegion: string;
+  company: string;
+  id: number;
+}
